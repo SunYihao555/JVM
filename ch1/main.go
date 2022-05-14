9a9d714e7413fa4cc1d9329a4d8ebe7fe5b4c2bc
sunyihao <1915257388@qq.com>
2022-05-14 15:42:38 +0800

    add

--- ch1/main.go
+++ ch1/main.go
@@ -1,6 +1,8 @@
 package main
 
 import "fmt"
+import "strings"
+import "JVM/ch1/classpath"
 
 func main() {
 	cmd := parseCmd()
@@ -13,6 +15,15 @@ func main() {
 	}
 
 }
-func startJVM(cmd *Cmd){
-	fmt.Printf("classpath:%s class:%s args:%v\n",cmd.cpOption,cmd.class,cmd.args)
+func startJVM(cmd *Cmd) {
+	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
+	fmt.Printf("classpath:%s class:%s args:%v\n", cmd.cpOption, cmd.class, cmd.args)
+	className := strings.Replace(cmd.class, ".", "/", -1)
+	classData, _, err := cp.ReadClass(className)
+	if err != nil {
+		fmt.Println("Could not find or load main class %s\n", cmd.class)
+		return
+	}
+	fmt.Printf("class data:%v\n", classData)
+
 }
